f30c272899f419ac1c3ffc27eb49589f25cf4c36
Cosmin <cosmin.barbascu@5psolutions.eu>
2020-08-25 18:40:39 +0300

    #9 orders.php http://pm.5psolutions.net/public/index.php?path_info=projects/training-cosmin-barbascu/tasks/9 Started order.php Almost done

--- order.php
+++ order.php
@@ -11,11 +11,16 @@ $stmt->bindValue(':id', $_GET['id'], PDO::PARAM_INT);
 $stmt->execute();
 $order = $stmt->fetch(PDO::FETCH_OBJ);
 
+$stmt = $pdo->prepare('SELECT * FROM orders_products INNER JOIN product ON product.id = orders_products.id_product WHERE orders_products.id_order = :id');
+$stmt->bindValue(':id', $_GET['id'], PDO::PARAM_INT);
+$stmt->execute();
+$products = $stmt->fetchAll(PDO::FETCH_OBJ);
+
+print_r($products);
+
 //if (!$order) {
 //    redirect('orders');
 //}
-
-
 
 ?>
 
@@ -33,6 +38,17 @@ $order = $stmt->fetch(PDO::FETCH_OBJ);
 <p><?= $order->name; ?></p>
 <p><?= $order->comments; ?></p>
 <a href="order.php?id= <?= $order->id; ?>">View</a>
+
+<?php foreach ($products as $product): ?>
+    <div style="display: flex; width: 700px; margin: auto">
+        <img src="<?= $product->img_path; ?>" alt="product image" style="width: 150px; height: 150px">
+        <div>
+            <h1><?= $product->title; ?></h1>
+            <p><?= $product->description; ?></p>
+            <p><?= $product->price; ?></p>
+        </div>
+    </div>
+<?php endforeach; ?>
 
 
 </body>
