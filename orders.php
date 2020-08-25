8320f92599c34400b32ef67dc6803bf1e8e63b20
Cosmin <cosmin.barbascu@5psolutions.eu>
2020-08-25 18:17:49 +0300

    #9 orders.php http://pm.5psolutions.net/public/index.php?path_info=projects/training-cosmin-barbascu/tasks/9 Small changes

--- orders.php
+++ orders.php
@@ -5,7 +5,7 @@ require_once 'common.php';
 
 $stmt = $pdo->prepare(
     'SELECT orders.id, orders.creation_date, orders.name, orders.contact_details, orders.comments, SUM(price) AS price
-     FROM orders INNER JOIN orders_products ON orders.id = orders_products.id_order 
+     FROM orders LEFT JOIN orders_products ON orders.id = orders_products.id_order 
      GROUP by orders.id, orders.creation_date, orders.name, orders.contact_details, orders.comments'
 );
 $stmt->execute();
@@ -22,16 +22,14 @@ $orders = $stmt->fetchAll(PDO::FETCH_OBJ);
     <title>Document</title>
 </head>
 <body>
-<?php
-foreach ($orders as $order): ?>
+<?php foreach ($orders as $order): ?>
     <div style="width: 700px; margin: auto; border: 1px black solid">
-        <p><?= $order->creation_date; ?></p><br>
-        <p><?= $order->name; ?></p><br>
-        <p><?= $order->comments; ?></p><br>
-        <p><?= $order->price; ?></p><br>
-        <a href="order.php?id= <?= $order->id; ?>">View</a>
+        <p><?= $order->creation_date; ?></p>
+        <p><?= $order->name; ?></p>
+        <p><?= $order->comments; ?></p>
+        <p><?= $order->price; ?></p>
+        <a href="order.php?id=<?= $order->id; ?>">View</a>
     </div>
-<?php
-endforeach; ?>
+<?php endforeach; ?>
 </body>
 </html>
